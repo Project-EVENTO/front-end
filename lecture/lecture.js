f7163f7e224de206a84977fcc6c9f8afce295ace
정우재 <80890472+woojae05@users.noreply.github.com>
2021-11-16 22:05:49 +0900

    upadate: lecture
    유효성검사

--- lecture/lecture.js
+++ lecture/lecture.js
@@ -1,15 +1,33 @@
-const Title = document.getElementById('title')
-const Host = document.getElementById('host')
-const Place =  document.getElementById('place')
-const Member =  document.getElementById('member')
 
-const regex = new RegExp(pattern,flag)
-const regex = 
+const title = document.querySelector("#title")
+const host =  document.querySelector("#host")
+const place = document.querySelector("#place")
+const grade =  document.querySelector("#grade")
+const date = document.querySelector("#date");
+const memberInput = document.querySelector("#member");
 
-function hi() {
-   
-} 
-
-
+function postData(){
+    document.lectureForm.submit();
+}
 
 
+const compare= ()=>{
+    console.log(title.value)
+    if(title.value==''){
+        swal("특강명을 입력해주세요.")
+    }else if(host.value===""){
+        swal("주최자를 입력해주세요.")
+    }else if(place.value===''){
+        swal("장소를 입력해주세요.")
+    }else if(grade.value==0){
+        swal("학년을 선택해주세요.")
+    }else if(memberInput.value==''){
+        swal("인원을 입력해주세요.")
+    }else if(date.value==''){
+        swal("날짜를 입력해주세요.")
+    }
+    else{
+        swal("특강 생성 성공","버튼을 눌러주세요.","succes");
+        postData();
+    }
+}
