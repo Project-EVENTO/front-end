285431b5173f5bb52abc93e93ec1e297d22b4932
정우재 <80890472+woojae05@users.noreply.github.com>
2021-11-30 21:40:16 +0900

    update:lectureList
    lectureList 수정기능

--- lecture/lectureList.js
+++ lecture/lectureList.js
@@ -2,6 +2,7 @@ const serverAddress = "http://10.80.162.177:5500";
 
 
 const table = document.querySelector("#lectureList")
+let fixing = true
 
 function getData(){
     axios.get(serverAddress+'/lecture')
@@ -16,22 +17,27 @@ function getData(){
 
 
 function lectureList(data){
-    data.map((ele)=>{ 
+    data.map((ele,index)=>{ 
         const tr = document.createElement("tr")
         const delbtn = document.createElement("button")
+        const updateBtn = document.createElement("button")
         Array('lc_title', 'lc_host', 'lc_place','lc_grade','lc_max','lc_date','lc_des' ).forEach((keyName) => {
-            console.log(keyName)
+            // console.log(keyName)
             const property = ele[keyName]
             const td  = document.createElement("td")
             td.innerText = property
             td.id = `ele${data.indexOf(ele)}_${keyName}`
-            console.log(td.id)
+            // console.log(td.id)
             tr.appendChild(td)
         })
         tr.id = `masterEle${data.indexOf(ele)}`
         delbtn.innerText="❌"
-        delbtn.id=ele['lc_seq']
+        updateBtn.innerText="🔧"
+        delbtn.id="del"+ele['lc_seq']
+        updateBtn.id="update"+ele['lc_seq']
         delbtn.addEventListener("click",delLecture)
+        updateBtn.addEventListener("click",function (){updateLecture(ele,index)})
+        tr.appendChild(updateBtn)
         tr.appendChild(delbtn)
         table.appendChild(tr)
         
@@ -41,11 +47,38 @@ function lectureList(data){
 function delLecture(event){
     const id = event.target.id
     axios.post(serverAddress+'/lecture/delete',{id:id})
-    .then((req)=>{
-        console.log(suc)
-        getData();
+    .then(()=>{
+        getData();  
     })
 }
 
+function updateLecture(ele,index){
+    console.log(ele)
+    console.log(index)
+    const Title = document.querySelector(`#ele${index}_lc_title`);
+    const Host = document.querySelector(`#ele${index}_lc_host`);
+    const Place = document.querySelector(`#ele${index}_lc_place`);
+    const Grade = document.querySelector(`#ele${index}_lc_grade`);
+    const Max = document.querySelector(`#ele${index}_lc_max`)
+    const Date = document.querySelector(`#ele${index}_lc_date`);
+    const Des = document.querySelector(`#ele${index}_lc_des`);
+    const tr = document.querySelector("tr")
+    const checkBtn = document.createElement("button")
+    const inputBox = document.createElement("input")    
+
+    inputBox.value = Title.childNodes[0].nodeValue  
+
+    Title.childNodes[0].nodeValue = "" 
+    Host.childNodes[0].nodeValue = "" 
+    Place.childNodes[0].nodeValue = "" 
+    Grade.childNodes[0].nodeValue = "" 
+    Max.childNodes[0].nodeValue = "" 
+    Date.childNodes[0].nodeValue = "" 
+    // Des.childNodes[0].nodeValue = "" 
+    
+    Title.appendChild(inputBox)
+    checkBtn.innerText="✅"
+    tr.appendChild(checkBtn)
+}
 
 getData();
